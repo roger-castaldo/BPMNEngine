436f9d56a375652c3277037e811cb2813e0ad358
roger-castaldo <roger.castaldo@gmail.com>
2017-10-20 16:20:10 -0400

    corrected bug with encoding animated gifs in mono (it exports gifs in 87a format).  Placed code to parse a stored gif and return only the relevant data for animating the requested process.

--- Drawing/GifEncoder.cs
+++ Drawing/GifEncoder.cs
@@ -16,6 +16,90 @@ namespace Org.Reddragonit.BpmEngine.Drawing
     /// </summary>
     public class GifEncoder : IDisposable
     {
+        private struct sGif
+        {
+            private short _canvasWidth;
+            public short CanvasWidth { get { return _canvasWidth; } }
+            private short _canvasHeight;
+            public short CanvasHeight { get { return _canvasHeight; } }
+            private byte _packedField;
+            public byte PackedField { get { return _packedField; } }
+            private byte[] _colorTable;
+            public byte[] ColorTable { get { return _colorTable; } }
+            private byte[] _data;
+            public byte[] Data { get { return _data; } }
+
+            private int _colorTableByteLength;
+            public int ColorTableByteLength { get { return _colorTableByteLength; } }
+
+            public sGif(Image img)
+            {
+                int len;
+                _canvasWidth = (short)img.Width;
+                _canvasHeight = (short)img.Height;
+                MemoryStream ms = new MemoryStream();
+                img.Save(ms, ImageFormat.Gif);
+                ms.Position = 0;
+                BinaryReader br = new BinaryReader(ms);
+                br.ReadBytes(4); //skip gif8
+                bool gif87a = !(br.ReadByte() == (byte)0x39);
+                br.ReadBytes(1); //skip remaining header
+                br.ReadBytes(4); //skip dimensions
+                _packedField = (byte)br.ReadByte();
+                br.ReadBytes(2); //skip background color and aspect ratio
+                int colorTableLength = 0;
+                if (!gif87a)
+                {
+                    _colorTableByteLength = (int)((int)_packedField & 0x07);
+                    colorTableLength = (int)Math.Pow(2, _colorTableByteLength + 1);
+                }
+                else
+                {
+                    _colorTableByteLength = (int)((int)_packedField & 0x07);
+                    colorTableLength = (int)Math.Pow(2, _colorTableByteLength + 1);
+                    _packedField = (byte)(0x80 | (((int)_packedField & 0x10) >> 1) | _colorTableByteLength);
+                }
+                _colorTable = br.ReadBytes(colorTableLength * 3);
+                if (!gif87a)
+                {
+                    while (br.ReadByte() == 0x21)
+                    {
+                        switch (br.ReadByte())
+                        {
+                            case 0xF9:
+                                len = br.ReadByte();
+                                br.ReadBytes(len); //skip gce content
+                                br.ReadByte(); //skip extension end
+                                break;
+                            case 0xFF:
+                            case 0x01:
+                                len = br.ReadByte();
+                                br.ReadBytes(len); //skip descriptor
+                                len = br.ReadByte(); //read len of next block
+                                while (len != 0)
+                                {
+                                    br.ReadBytes(len); //skip block content
+                                    len = br.ReadByte(); //read len of next block
+                                }
+                                break;
+                        }
+                    }
+
+                }
+                else
+                    br.ReadByte(); //skip image sperator char
+                br.ReadBytes(8); //skip dimensions/image seperator/positions
+                byte tb = br.ReadByte();
+                if (((int)tb & 0x80) == (int)0x80)
+                {
+                    len = (int)Math.Pow(2, ((int)tb & 0x07) + 1);
+                    _colorTableByteLength = tb & 0x07;
+                    _colorTable = br.ReadBytes(len * 3);
+                }
+                _data = br.ReadBytes((int)(br.BaseStream.Length - br.BaseStream.Position - 1));
+            }
+        }
+
         #region Header Constants
         private const string FileType = "GIF";
         private const string FileVersion = "89a";
@@ -27,20 +111,10 @@ namespace Org.Reddragonit.BpmEngine.Drawing
 
         private const int GraphicControlExtensionBlockIdentifier = 0xf921;
         private const byte GraphicControlExtensionBlockSize = 0x04;
-
-        private const long SourceGlobalColorInfoPosition = 10;
-        private const long SourceGraphicControlExtensionPosition = 781;
-        private const long SourceGraphicControlExtensionLength = 8;
-        private const long SourceImageBlockPosition = 789;
-        private const long SourceImageBlockHeaderLength = 11;
-        private const long SourceColorBlockPosition = 13;
-        private const long SourceColorBlockLength = 768;
-        #endregion
+        private const byte ImageSeperator = 0x2C;
+#endregion
 
         private bool _isFirstImage = true;
-        private int? _width;
-        private int? _height;
-        private int? _repeatCount;
         private readonly Stream _stream;
 
         // Public Accessors
@@ -55,12 +129,9 @@ namespace Org.Reddragonit.BpmEngine.Drawing
         /// <param name="stream">The stream that will be written to.</param>
         /// <param name="width">Sets the width for this gif or null to use the first frame's width.</param>
         /// <param name="height">Sets the height for this gif or null to use the first frame's height.</param>
-        public GifEncoder(Stream stream, int? width = null, int? height = null, int? repeatCount = null)
+        public GifEncoder(Stream stream)
         {
             _stream = stream;
-            _width = width;
-            _height = height;
-            _repeatCount = repeatCount;
         }
 
         /// <summary>
@@ -69,33 +140,32 @@ namespace Org.Reddragonit.BpmEngine.Drawing
         /// <param name="img">The image to add</param>
         /// <param name="x">The positioning x offset this image should be displayed at.</param>
         /// <param name="y">The positioning y offset this image should be displayed at.</param>
-        public void AddFrame(Image img, int x = 0, int y = 0, TimeSpan? frameDelay = null)
+        public void AddFrame(Image img)
         {
             using (var gifStream = new MemoryStream())
             {
-                img.Save(gifStream, ImageFormat.Gif);
+                sGif gif = new sGif(img);
                 if (_isFirstImage) // Steal the global color table info
                 {
-                    InitHeader(gifStream, img.Width, img.Height);
+                    InitHeader(gif);
                 }
-                WriteGraphicControlBlock(gifStream, frameDelay.GetValueOrDefault(FrameDelay));
-                WriteImageBlock(gifStream, !_isFirstImage, x, y, img.Width, img.Height);
+                WriteGraphicControlBlock(gif, FrameDelay);
+                WriteImageBlock(gif, !_isFirstImage, 0, 0);
             }
             _isFirstImage = false;
         }
 
-        private void InitHeader(Stream sourceGif, int w, int h)
+        private void InitHeader(sGif gif)
         {
             // File Header
             WriteString(FileType);
             WriteString(FileVersion);
-            WriteShort(_width.GetValueOrDefault(w)); // Initial Logical Width
-            WriteShort(_height.GetValueOrDefault(h)); // Initial Logical Height
-            sourceGif.Position = SourceGlobalColorInfoPosition;
-            WriteByte(sourceGif.ReadByte()); // Global Color Table Info
+            WriteShort(gif.CanvasWidth); // Initial Logical Width
+            WriteShort(gif.CanvasHeight); // Initial Logical Height
+            WriteByte(gif.PackedField); // Global Color Table Info
             WriteByte(0); // Background Color Index
             WriteByte(0); // Pixel aspect ratio
-            WriteColorTable(sourceGif);
+            WriteByte(gif.ColorTable);
 
             // App Extension Header
             WriteShort(ApplicationExtensionBlockIdentifier);
@@ -103,77 +173,46 @@ namespace Org.Reddragonit.BpmEngine.Drawing
             WriteString(ApplicationIdentification);
             WriteByte(3); // Application block length
             WriteByte(1);
-            WriteShort(_repeatCount.GetValueOrDefault(0)); // Repeat count for images.
+            WriteShort(0); // Repeat count for images.
             WriteByte(0); // terminator
         }
 
-        private void WriteColorTable(Stream sourceGif)
+        private void WriteGraphicControlBlock(sGif gif, TimeSpan frameDelay)
         {
-            sourceGif.Position = SourceColorBlockPosition; // Locating the image color table
-            var colorTable = new byte[SourceColorBlockLength];
-            sourceGif.Read(colorTable, 0, colorTable.Length);
-            _stream.Write(colorTable, 0, colorTable.Length);
-        }
-
-        private void WriteGraphicControlBlock(Stream sourceGif, TimeSpan frameDelay)
-        {
-            sourceGif.Position = SourceGraphicControlExtensionPosition; // Locating the source GCE
-            var blockhead = new byte[SourceGraphicControlExtensionLength];
-            sourceGif.Read(blockhead, 0, blockhead.Length); // Reading source GCE
-
             WriteShort(GraphicControlExtensionBlockIdentifier); // Identifier
             WriteByte(GraphicControlExtensionBlockSize); // Block Size
-            WriteByte(blockhead[3] & 0xf7 | 0x08); // Setting disposal flag
+            WriteByte(0x00);
             WriteShort(Convert.ToInt32(frameDelay.TotalMilliseconds / 10)); // Setting frame delay
-            WriteByte(blockhead[6]); // Transparent color index
+            WriteByte(0); // Transparent color index
             WriteByte(0); // Terminator
         }
 
-        private void WriteImageBlock(Stream sourceGif, bool includeColorTable, int x, int y, int h, int w)
+        private void WriteImageBlock(sGif gif, bool includeColorTable, int x, int y)
         {
-            sourceGif.Position = SourceImageBlockPosition; // Locating the image block
-            var header = new byte[SourceImageBlockHeaderLength];
-            sourceGif.Read(header, 0, header.Length);
-            WriteByte(header[0]); // Separator
+            WriteByte(ImageSeperator); // Separator
             WriteShort(x); // Position X
             WriteShort(y); // Position Y
-            WriteShort(h); // Height
-            WriteShort(w); // Width
+            WriteShort(gif.CanvasWidth); 
+            WriteShort(gif.CanvasHeight);
 
             if (includeColorTable) // If first frame, use global color table - else use local
             {
-                sourceGif.Position = SourceGlobalColorInfoPosition;
-                WriteByte(sourceGif.ReadByte() & 0x3f | 0x80); // Enabling local color table
-                WriteColorTable(sourceGif);
+                WriteByte((((int)gif.PackedField&0x70) >> 4) | 0x80); // Enabling local color table
+                WriteByte(gif.ColorTable);
             }
             else
-            {
-                WriteByte(header[9] & 0x07 | 0x07); // Disabling local color table
-            }
-
-            WriteByte(header[10]); // LZW Min Code Size
-
-            // Read/Write image data
-            sourceGif.Position = SourceImageBlockPosition + SourceImageBlockHeaderLength;
-
-            var dataLength = sourceGif.ReadByte();
-            while (dataLength > 0)
-            {
-                var imgData = new byte[dataLength];
-                sourceGif.Read(imgData, 0, dataLength);
-
-                _stream.WriteByte(Convert.ToByte(dataLength));
-                _stream.Write(imgData, 0, dataLength);
-                dataLength = sourceGif.ReadByte();
-            }
-
-            _stream.WriteByte(0); // Terminator
-
+                WriteByte(0); // Disabling local color table
+            WriteByte(gif.Data);
         }
 
         private void WriteByte(int value)
         {
             _stream.WriteByte(Convert.ToByte(value));
+        }
+
+        private void WriteByte(byte[] data)
+        {
+            _stream.Write(data, 0, data.Length);
         }
 
         private void WriteShort(int value)
